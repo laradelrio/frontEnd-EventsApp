dee8b5a8893bf65c8b1da64f133ce1eb32e9e031
laradelrio <lara.delrio333@gmail.com>
2023-11-05 20:03:29 +0100

    home page

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Event } from 'src/app/data/interfaces/interfaces.interface';
+import { EventDbApiService } from 'src/app/data/services/api/event-db-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  events: Event[] = []
+
+  constructor(
+    private eventService: EventDbApiService,) {
+
+  }
+  ngOnInit(): void {
+    this.getUserEvents()
+  }
+
+  getUserEvents() {
+    this.eventService.getAllEvents()
+      .subscribe((res) => {
+        res.data.forEach((event) => this.events.push(event))
+      })
+  }
 }
