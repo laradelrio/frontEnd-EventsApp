108bb1ef7d36f1c04666461b1b5b7e0bca56ceb1
laradelrio <lara.delrio333@gmail.com>
2024-04-03 17:30:42 +0200

    git backend end ppint

--- src/app/data/constants/constants.ts
+++ src/app/data/constants/constants.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 @Injectable()
 export class Constants {
-    public static DB_API_ENDPOINT: string = 'https://backend-eventsapp.onrender.com';
+    public static DB_API_ENDPOINT: string = 'https://backend-eventsapp.onrender.com/';
     public static IBB_API_URL: string = 'https://api.imgbb.com/1/upload'
     public static TitleOfSite: string = "Events";
 
